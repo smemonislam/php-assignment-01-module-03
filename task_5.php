0b0f510cfe98d3a154b6cffd0a35596753787855
SM Emon Islam <73327452+smemonislam@users.noreply.github.com>
2023-09-26 00:58:19 +0600

    Change count of length, divided string and combine string, every line comment

--- task_5.php
+++ task_5.php
@@ -1,14 +1,28 @@
 <?php
-function generatePassword ($length){
+function generatePassword($length)
+{
 
-    
-    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_+=';
+    // Define character sets for different types of characters
+    $lowercase = 'abcdefghijklmnopqrstuvwxyz';
+    $uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    $numbers = "0123456789";
+    $special_characters = "!@#$%^&*()-_+=";
+
+    // Combine all character
+    $characters = $lowercase . $uppercase . $numbers . $special_characters;
+
+    // Get the total number of characters
+    $len = strlen($characters);
+
+    // Initialize the password variable
     $password = "";
 
-    for ($i=0; $i <=$length ; $i++) { 
-        $password .= $characters[rand(0, strlen($characters)-1)];
+    // Generate the password
+    for ($i = 0; $i <= $length; $i++) {
+        $password .= $characters[rand(0, $len - 1)];
     }
+
     return $password;
 }
-
-echo generatePassword(10);
+// Generate a 12-character password
+echo generatePassword(12);
