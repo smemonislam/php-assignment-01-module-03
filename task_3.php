0f4fb7e1edfa39102b5cf04205830e1314dde88a
SM Emon Islam <73327452+smemonislam@users.noreply.github.com>
2023-09-26 18:55:50 +0600

    Update Task 3

--- task_3.php
+++ task_3.php
@@ -8,32 +8,49 @@
  */
 
 $grades = [85, 92, 78, 88, 95];
-function sortedByDescendingOrder($grades)
+
+
+// Function to compare grades in descending order
+function sortedByDescendingOrder($a, $b)
 {
-    // Create an empty array to store the result
-    $newArray = array();
-
-    // Descending order by rsort() function 
-    rsort($grades);
-    foreach ($grades as $value) {
-        array_push($newArray, $value);
+    if ($a == $b) {
+        return 0;
     }
-
-    // Return descending the array
-    return $newArray;
+    return ($a > $b) ? -1 : 1;
 }
 
-$result = sortedByDescendingOrder($grades);
+usort($grades, 'sortedByDescendingOrder');
 
 // Print the sorted grades as an array
-print_r($result);
+print_r($grades);
+
+
+// function sortedByDescendingOrder($grades)
+// {
+//     // Create an empty array to store the result
+//     $newArray = array();
+
+//     // Descending order by rsort() function 
+//     arsort($grades);
+//     foreach ($grades as $value) {
+//         array_push($newArray, $value);
+//     }
+
+//     // Return descending the array
+//     return $newArray;
+// }
+
+// $result = sortedByDescendingOrder($grades);
+
+// // Print the sorted grades as an array
+// print_r($result);
 
 
 
 // function sortedByDescendingOrder($grades)
 // {
 //     // Sort the grades in descending order
-//     rsort($grades);
+//     arsort($grades);
 
 //     // Print the sorted grades as an array
 //     print_r($grades);
